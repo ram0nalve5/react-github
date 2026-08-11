6f10e6289669c7d6d61dc7f0028ee2775188edbb
ram0nalve5 <ramon.alves906900@gmail.com>
2026-08-11 08:10:30 -0300

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 // Importação das páginas do currículo
 import Index from './pages/Index.jsx';
